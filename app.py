63d5d5a3b9b719f53edcee570e7512a3658a9728
zacajaca21 <zacajaca21@gmail.com>
2025-02-25 10:32:22 -0300

    add - datetime in python

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_page():
     return render_template("index.html")
+
+@app.route("/post/menssagem", methods=["POST"])
+def cadastra_ms():
+
+    usuario = request.form.get("nome")
+    menssagem = request.form.get("comentario")
+    data_hora = datetime.datetime.now()
+    
+    return redirect("/")
 
 app.run(debug=True)
