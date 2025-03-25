15bc700511a305c3f4695cea256fa24462ee1f38
zacajaca21 <zacajaca21@gmail.com>
2025-03-25 11:31:48 -0300

    add - criar usuario

--- app.py
+++ app.py
@@ -32,18 +32,25 @@ def curtidas_del(curtidas):
     Mensagem.del_curtidas(curtidas)
     return redirect("/mensagem")
 
-@app.route("/cadastra/usuario")
-def cadastra_usuario(email, usuario, senha):
+@app.route("/login")
+def pag_login():
+    return render_template("login.html")
+
+@app.route("/usuario")
+def usuario():
+    return render_template("cadastro.html")
+
+@app.route("/cadastra/usuario", methods=["POST"])
+def cadastra_usuario():
+    
     email = request.form.get("email")
     usuario = request.form.get("usuario")
     senha = request.form.get("senha")
 
     Mensagem.cadastro_usuario(email, usuario, senha)
-    return redirect("/")
+    return redirect("/usuario")
 
-@app.route("/login")
-def pag_login():
-    return render_template("login.html")
+
 
 if __name__ == __name__:
     app.run(debug=True)
